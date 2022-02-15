840348b262158c7f2bcc64408fa01b080f6a3ac8
Jared Medeiros <jared14jared@gmail.com>
2022-02-15 14:55:08 -0500

    added a script that translates text, added a landing page, added styling to all pages. final iteration before presentations

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,4 +1,41 @@
+// const { type } = require("express/lib/response");
+
+function generateText(x) {
+    let resultsContainer = document.querySelector('.result-box');
+    let pTag = document.createElement('p');
+    pTag.innerText = x;
+
+    resultsContainer.appendChild(pTag)
+}
+
+
+
+function translateWord (str, lang) {
+
+    const options = {
+    method: 'POST',
+    url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2',
+    headers: {
+    'content-type': 'application/json',
+    'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
+    'x-rapidapi-key': 'cd85d7f71bmsh508b6b64dff8da8p1b943cjsnc3090c186953'
+    },
+        data: {q: `${str}`, target: `${lang}`}
+    };
+
+    axios.request(options).then(function (response) {
+    generateText(response.data.data.translations.translatedText)
+    console.log(response.data.data.translations.translatedText);
+
+    }).catch(function (error) {
+	    console.error(error);
+    });
+}
+
+let transcript;
+
 function runSpeechRecognition() {
+    // let final_transcript = "";
 	// store our output div as a variable
 	let output = document.getElementById("output");
 	// store our action as a variable
@@ -20,35 +57,193 @@ function runSpeechRecognition() {
               
     // Run this function when the speech recogition service returns a result
     recognition.onresult = function(event) {
-        let transcript = event.results[0][0].transcript;
-        //add this result as inner text into our output, and remove the "hide" class so that it is now visible
+        
+        transcript = event.results[0][0].transcript;       
+        console.log(transcript)
         output.innerText = transcript;
+        // final_transcript = event.results[0][0];
+        // text.push(transcript)
         output.classList.remove("hide");
-                    
-    };
-              
-     // Start the voice recognition
-        recognition.start();
-}
+        let languagecontainer = document.getElementById("languages-container")
+        languagecontainer.classList.remove("hide");
+        
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
-	"method": "POST",
-	"headers": {
-		"content-type": "application/x-www-form-urlencoded",
-		"accept-encoding": "application/gzip",
-		"x-rapidapi-host": "google-translate1.p.rapidapi.com",
-		"x-rapidapi-key": "e24289a261msh4ef0b0fb387853bp13e458jsn9d39a10e984a"
-	},
-	"data": {
-		"q": "Hello, world!",
-		"target": "es",
-		"source": "en"
-	}
+    };
+    // translateWord('hello', 'es');
+
+ 
+    recognition.start();
+    // return final_transcript;
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+let listItem = document.querySelectorAll('.language-item');
+
+
+
+// // // //function to get last two in array: lan code, last two used to search
+listItem.forEach(function(element) {
+    element.addEventListener('click', function () {
+        let strArr = element.innerText.split(':');
+        let code = strArr[strArr.length - 1];
+        code = code.trim();
+        translateWord(transcript, code);
+    })
+})
+
+
+
+
+// let languageSection = document.querySelector('.display-languages');
+
+//where list of languages are being added
+// let languageList = document.querySelector('.language');
+
+// // console.log(languageList)
+
+//translates the word.
+
+
+
+
+
+
+
+
+
+// function translateWord (str, lang) {
+
+//     const options = {
+//     method: 'POST',
+//     url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2',
+//     headers: {
+//     'content-type': 'application/json',
+//     'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
+//     'x-rapidapi-key': 'cd85d7f71bmsh508b6b64dff8da8p1b943cjsnc3090c186953'
+//     },
+//         data: {q: `${str}`, target: `${lang}`}
+//     };
+
+//     axios.request(options).then(function (response) {
+// 	    console.log(response.data.data.translations.translatedText);
+//     }).catch(function (error) {
+// 	    console.error(error);
+//     });
+// }
+
+
+        // translateWord('hello', 'es');
+
+
+// retrieve options and display them.
+const options = {
+    method: 'GET',
+    url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2/languages',
+    headers: {
+    'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
+    'x-rapidapi-key': 'cd85d7f71bmsh508b6b64dff8da8p1b943cjsnc3090c186953'
+    }
+};
+
+axios.request(options).then(function (response) {
+	let languageData = response.data.languages;
+
+    for (let i = 0;  i < languageData.length; i++) {
+        let lanCode = languageData[i].language;
+        let languageName = languageData[i].name;
+
+
+        let lanType = document.createElement('li');
+        lanType.className = 'language-item'
+        lanType.innerText = languageName + " : " + lanCode
+
+        languageList.append(lanType)
+
+    }
+
+}).catch(function (error) {
+	console.error(error);
 });
+
+
+// let resultsContainer = document.querySelector('.result-box');
+// console.log(resultsContainer) 
+
+
+// // functions to display text on screen, will use function above to translate;
+// // axios function;
+// function generateText(x,y) {
+//     let resultsContainer = document.querySelector('.result-box');
+//     let pTag = document.createElement('p');
+//     pTag.innerText = x + " ; " + y
+
+//     resultsContainer.appendChild(pTag)
+// }
+
+
+// // //selects all list items and retrives value;
+// let listItem = document.querySelectorAll('.language-item');
+
+
+
+// // // // //function to get last two in array: lan code, last two used to search
+// listItem.forEach(function(element) {
+//     element.addEventListener('click', function () {
+//         let strArr = element.innerText.split(':');
+//         let code = strArr[strArr.length - 1];
+//         // generateText('hello', code)
+//         console.log(code)
+//         console.log(transcript)
+//     })
+// })​​​
+
+
+
+
+// function getLanguages () {
+//     const options = {
+//       method: 'GET',
+//       url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2/languages',
+//       headers: {
+//         'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
+//         'x-rapidapi-key': 'cd85d7f71bmsh508b6b64dff8da8p1b943cjsnc3090c186953'
+//       }
+//     };
+
+//     axios.request(options).then(function (response) {
+//     	let langArr = response.data.languages;
+        
+//         langArr.forEach(element => {
+//             console.log(element)
+//         });
+
+        
+//     }).catch(function (error) {
+//     	console.error(error);
+//     });
+// };
+
+
+// // getLanguages()
+
+
+// function translateWord (str, lang) {
+
+//     const options = {
+//     method: 'POST',
+//     url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2',
+//     headers: {
+//     'content-type': 'application/json',
+//     'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
+//     'x-rapidapi-key': 'cd85d7f71bmsh508b6b64dff8da8p1b943cjsnc3090c186953'
+//   },
+//     data: {q: `${str}`, target: `${lang}`}
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data.data.translations.translatedText);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+// }
+
+// translateWord('hello', 'es')
