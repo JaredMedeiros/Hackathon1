0bddf80f89eed052ebb0fa73ea3a324cbcaaa37e
Jared Medeiros <jared14jared@gmail.com>
2022-02-15 00:43:34 -0500

    javascript file and pseudo code polished, first round of styling added to the interface page.

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,31 +1,54 @@
-/* JS comes here */
-		    function runSpeechRecognition() {
-		        // get output div reference
-		        let output = document.getElementById("output");
-		        // get action element reference
-		        let action = document.getElementById("action");
-                // new speech recognition object
-                var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-                var recognition = new SpeechRecognition();
+function runSpeechRecognition() {
+	// store our output div as a variable
+	let output = document.getElementById("output");
+	// store our action as a variable
+	 let action = document.getElementById("action");
+    // create a new speech recognition obtion
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
             
-                // This runs when the speech recognition service starts
-                recognition.onstart = function() {
-                    action.innerText = "listening...";
-                };
+     // Run this function when the speech recognition begins
+    recognition.onstart = function() {
+        action.innerText = "listening...";
+    };
                 
-                recognition.onspeechend = function() {
-                    action.innerText = "done! generating text...";
-                    recognition.stop();
-                }
+    //Recognize when the user is finished speaking and run this function generate a response
+    recognition.onspeechend = function() {
+        action.innerText = "done! generating text...";
+        recognition.stop();
+    }
               
-                // This runs when the speech recognition service returns result
-                recognition.onresult = function(event) {
-                    let transcript = event.results[0][0].transcript;
-                    output.innerText = transcript;
-                    output.classList.remove("hide");
+    // Run this function when the speech recogition service returns a result
+    recognition.onresult = function(event) {
+        let transcript = event.results[0][0].transcript;
+        //add this result as inner text into our output, and remove the "hide" class so that it is now visible
+        output.innerText = transcript;
+        output.classList.remove("hide");
                     
-                };
+    };
               
-                 // start recognition
-                 recognition.start();
-	        }
+     // Start the voice recognition
+        recognition.start();
+}
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"accept-encoding": "application/gzip",
+		"x-rapidapi-host": "google-translate1.p.rapidapi.com",
+		"x-rapidapi-key": "e24289a261msh4ef0b0fb387853bp13e458jsn9d39a10e984a"
+	},
+	"data": {
+		"q": "Hello, world!",
+		"target": "es",
+		"source": "en"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
